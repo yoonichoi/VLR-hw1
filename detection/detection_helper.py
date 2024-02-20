10cdf4154434db5c39bfacb4933de147c1f9a8b2
Yooni <yooni.jy.choi@gmail.com>
2024-02-20 21:09:48 +0000

    add total loss to tensorboard

--- detection/detection_helper.py
+++ detection/detection_helper.py
@@ -232,6 +232,7 @@ def train_detector(
         lr_scheduler.step()
         for key, value in losses.items():
             writer.add_scalar("train/" + key + "_{}".format("overfit" if overfit else "full"), value, _iter)
+        writer.add_scalar("train/loss_total" + "_{}".format("overfit" if overfit else "full"), total_loss, _iter)
         # Print losses periodically.
         if _iter % log_period == 0:
             loss_str = f"[Iter {_iter}][loss: {total_loss:.3f}]"
